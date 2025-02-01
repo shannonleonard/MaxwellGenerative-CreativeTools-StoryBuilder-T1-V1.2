9cd52100193b57e9018b46915fbbb4ffa521c24b
Shannon Leonard <121053869+shannonleonard@users.noreply.github.com>
2025-02-01 13:24:13 -0800

    Fix deployment configuration

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import VerticalSlideshow from './VerticalSlideshow';
 
 function App() {
   return (
-    <div className="App min-h-screen bg-gray-900">
+    <div className="App min-h-screen bg-gray-900" basename="/MaxwellGenerative-CreativeTools-StoryBuilder-T1-V1.0">
       <VerticalSlideshow />
     </div>
   );
